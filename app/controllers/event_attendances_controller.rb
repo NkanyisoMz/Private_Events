f606bd840f824c395d13cb68d27b33db0df64cbb
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-04-01 01:26:33 +0200

    Add attendance logic

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,7 +1,11 @@
 class EventAttendancesController < ApplicationController
-  def create
-  end
+  before_action :authenticate_user!
 
-  def destroy
+  def create
+    event = Event.find(params[:event_id])
+    unless event.attendees.include?(current_user)
+      event.attendees << current_user
+    end
+    redirect_to event, notice: "You are now attending this event!"
   end
 end
