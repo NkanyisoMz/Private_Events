969ab9e265e70caab775bec5f1af597dfca5bba1
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-04-03 12:59:04 +0200

    Update users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @upcoming_attended_events = @user.attended_events.upcoming
+    @past_attended_events = @user.attended_events.past
   end
 end
