353f37c4dd9980138bb32822f2d268b920bd3b3e
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-04-03 12:49:07 +0200

    Add class methods to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,14 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendances
-  has_many :attendees, through: :event_attendances, source: :attendees
+  has_many :attendees, through: :event_attendances, source: :attendee
+
+  # Class methods for filtering past and upcoming events
+  def self.past
+    where("date < ?", Date.today)
+  end
+
+  def self.upcoming
+    where("date >= ?", Date.today)
+  end
 end
