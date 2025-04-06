124cba9fe7b440a78a0989f1d0d9a7e26d69d3b7
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-04-06 23:48:17 +0200

    Corrent association of event from user to attendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendances
-  has_many :attendees, through: :event_attendances, source: :user
+  has_many :attendees, through: :event_attendances, source: :attendee
 
   has_many :invitations
   has_many :invited_users, through: :invitations, source: :user
