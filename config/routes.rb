67032c08f54771035a56a373f7b9b4c2b7716253
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-04-06 16:37:27 +0200

    Fix events routes to add all actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,11 @@
 Rails.application.routes.draw do
-  get "event_attendances/create"
-  get "event_attendances/destroy"
-  get "users/show"
   devise_for :users
-  get "users/sign_out", to: redirect("/")
-  delete "events/:event_id/unattend", to: "event_attendances#destroy", as: :unattend_event
-  get "events/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  resources :users, only: [ :show ]
-  resources :events, only: [ :index, :show, :new, :create ]
-  resources :event_attendances, only: [ :create ]
   root "events#index"
+
+  resources :users, only: [ :show ]
+  resources :events
+  resources :event_attendances, only: [ :create ]
+
+  delete "events/:event_id/unattend", to: "event_attendances#destroy", as: :unattend_event
 end
