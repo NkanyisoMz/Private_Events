c0812929d93cce4999ef35b98f7b7d0cfdaff958
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-04-06 15:32:16 +0200

    Allow add and edit only by creator

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
+  before_action :set_event, only: %i[edit update destroy]
+  before_action :authorize_creator!, only: %i[edit update destroy]
 
   def index
     @upcoming_events = Event.upcoming
@@ -24,9 +26,33 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: "Event updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, notice: "Event deleted!"
+  end
+
   private
 
   def event_params
     params.require(:event).permit(:title, :location, :date)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def authorize_creator!
+    redirect_to events_path, alert: "You are not authorized!" unless @event.creator == current_user
   end
 end
